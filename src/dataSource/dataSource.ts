39fb6ae95b7559c3e09768c14fbacb163eee5ff1
Jholman <jdreinat@geosat.com.co>
2024-07-06 15:15:37 -0500

    fix: port in env variables

--- src/dataSource/dataSource.ts
+++ src/dataSource/dataSource.ts
@@ -1,11 +1,11 @@
 import { DataSource } from "typeorm";
 import { entities } from "./entities";
-import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USERNAME } from "../utils/constants";
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME } from "../utils/constants";
 
 const appDataSource = new DataSource({
   type: "postgres",
   host: DB_HOST,
-  port: 5432,
+  port: DB_PORT,
   username: DB_USERNAME,
   password: DB_PASSWORD,
   database: DB_DATABASE,
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -16,3 +16,4 @@ export const DB_HOST = process.env.DB_HOST || '';
 export const DB_USERNAME = process.env.DB_USERNAME || ''; 
 export const DB_PASSWORD = process.env.DB_PASSWORD || '';
 export const DB_DATABASE = process.env.DB_DATABASE || '';
+export const DB_PORT = Number(process.env.DB_PORT) || 0;
